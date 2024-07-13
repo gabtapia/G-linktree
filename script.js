dc0eda7386ba7e6a26d4ebc08eb80d7338f1e5e0
gabtapia <gabrielnobretapia@gmail.com>
2024-07-13 18:27:07 -0300

    Corrigindo Bug da foto de perfil no JavaScript

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
     html.classList.toggle("light");
 
     if (html.classList.contains("light")) {
-        img.setAttribute("src", "./assets/avatar-light.png");
+        img.setAttribute("src", "./Assets/avatar-light.png");
         img.setAttribute("alt", "Foto do Gabriel de óculos de sol olhando pro lado")
     } else {
-        img.setAttribute("src", "./assets/avatar.png")
+        img.setAttribute("src", "./Assets/avatar.png")
         img.setAttribute("alt", "Foto do Gabriel de óculos olhando pro lado")
         
     }
